47fe3ee166a126a0596efc527f4b58882be9781b
chris woodle <chris@droplit.io>
2018-05-04 02:57:26 -0400

    Added lidar call

--- lidar.js
+++ lidar.js
@@ -1,8 +1,18 @@
-const scanDistances = () => {
-    return { front, right, back, left }
+const spawn = require('child_process').spawnSync;
+
+const scan = () => {
+    const results = spawn('python', ['rplidar.py', 'out.txt']);
+    const parts = results.split(' ');
+    const front = parts[0];
+    const right = parts[1];
+    const back = parts[2];
+    const left = parts[3];
+    const a = parts[4];
+    const b = parts[5];
+    const c = parts[6];
+    return { front, right, back, left, a, b, c };
 };
 
 module.exports = {
-    scanDistances: scanDistances,
-    // scanAngles: scanAngles
+    scan: scan,
 }
