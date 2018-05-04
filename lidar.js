254c3d8dd4897b33e2bd658b66d3cb80717cbd98
chris woodle <chris@droplit.io>
2018-05-04 04:39:06 -0400

    fix

--- lidar.js
+++ lidar.js
@@ -2,8 +2,8 @@ const spawn = require('child_process').spawnSync;
 
 const scan = () => {
     const results = spawn('python', ['/home/pi/robotics/rplidarbasic.py', 'out.txt']);
-    const parts = results.output.toString().trim().split(':');
-    const front = parts[0];
+    const parts = results.output.toString().trim().split('');
+    const front = parts[0].substring(1);
     const right = parts[1];
     const back = parts[2];
     const left = parts[3];
